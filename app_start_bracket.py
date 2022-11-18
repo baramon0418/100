5eab75be50b38c5779bae7a5fce2e2835d16bd1b
baramon0418 <108270144+baramon0418@users.noreply.github.com>
2022-11-18 15:14:26 +0900

    Add files via upload

---
+++ app_start_bracket.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import RPi.GPIO as GPIO
+app = Flask(__name__)
+LED = 4
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
+@app.route("/")
+def helloworld():
+    return "Hello World"
+    
+@app.route("/led/<state>")
+def led(state):
+    if state == "on":
+        GPIO.output(LED, GPIO.HIGH)
+        return "LED ON"
+    else:
+        GPIO.output(LED, GPIO.LOW)
+        return "LED OFF"
+
+@app.route("/gpio/cleanup")
+def gpio_cleanup():
+    GPIO.cleanup()
+    return "GPIO CLEANUP"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
